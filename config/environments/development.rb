b5639790e381606a40afc0d9f64ce5421811dea3
Ben Fred Kutwa <kutwaben@gmail.com>
2017-05-31 22:08:27 +0300

    Add account activation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,10 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
+    config.action_mailer.raise_delivery_errors = true
+    config.action_mailer.delivery_method = :test
+    host = 'localhost:3000'  # Don't use this literally; use your local dev host instead
+    config.action_mailer.default_url_options = { host: host, protocol: 'http' }
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
